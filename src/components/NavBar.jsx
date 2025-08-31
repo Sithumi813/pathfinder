13ef61ce23e00cff9c38382328075d7704bfae1c
Sithumi813 <sithumisanjana813@gmail.com>
2025-08-31 11:35:43 +0530

    Change the navbar layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,17 @@
 // src/components/NavBar.jsx
 import React from "react";
+import Logo from "../images/logo.png";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from "./NavbarElements";
 
 export default function NavBar() {
   const nav = useNavigate();
@@ -16,18 +25,23 @@ export default function NavBar() {
   };
 
   return (
-    <nav style={{ padding: 12, background: "#fff", borderBottom: "1px solid #eee" }}>
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", maxWidth: 1100, margin: "0 auto" }}>
-        <div style={{ fontWeight: 700 }}>Path Finder</div>
-        <div style={{ display: "flex", gap: 12 }}>
-          <Link to="/dashboard">Dashboard</Link>
-          <Link to="/courses">Courses</Link>
-          <Link to="/recommendations">Recommendations</Link>
-          <Link to="/calendar">Calendar</Link>
-          <Link to="/profile">Profile</Link>
-          <button onClick={logout}>Logout</button>
-        </div>
-      </div>
-    </nav>
+    <Nav>
+      <NavLink to="/">
+        <img src={Logo} alt="logo" />
+      </NavLink>
+      <Bars />
+      <NavMenu>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/courses">Courses</NavLink>
+        <NavLink to="/recommendations">Recommendations</NavLink>
+        <NavLink to="/calendar">Calendar</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
+      </NavMenu>
+      <NavBtn>
+        <NavBtnLink as="button" onClick={logout}>
+          Logout
+        </NavBtnLink>
+      </NavBtn>
+    </Nav>    
   );
 }
