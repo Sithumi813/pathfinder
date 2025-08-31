6513547ee67e1dd6d7f4f290e54c0c30d78b6830
Sithumi813 <sithumisanjana813@gmail.com>
2025-08-31 21:54:46 +0530

    Decorate button

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -123,8 +123,8 @@ export default function Dashboard() {
                   }}
                 >
                   {completedCoursesSet.has(course.id)
-                    ? "✅ Completed"
-                    : "Mark Complete"}
+                    ? "Completed"
+                    : "Not Completed"}
                 </button>
               </div>
             ))}
